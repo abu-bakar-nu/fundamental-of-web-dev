b655d84c60105679e61816d71a63177c91f2138b
abu-bakar-nu <abubakar504@hotmail.com>
2021-01-07 10:04:57 -0600

    initial commit hw3

---
+++ Day 3/Homework3/src/app.js
@@ -0,0 +1,40 @@
+///Importing the required modules
+import express from "express";
+import bodyParser from "body-parser";
+//Mongoose is the tool we use to interact with the database
+import mongoose from "mongoose";
+
+///Create the express server.
+const app = express();
+
+//These are middleware to help parse data.
+app.use(express.json());
+app.use( bodyParser.json());
+app.use(express.static("public"));
+
+
+
+//This is the initialization of the database where we connect to the database
+mongoose.connect("paste your link here", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connected')
+});
+
+
+///These are the routes accesses, they are linked to the folders with the routes
+app.use('/api', require('./routes/authorRoutes'))
+app.use('/api', require('./routes/categoryRoutes'))
+app.use('/api', require('./routes/articleRoutes'))
+
+///The endpoint listening for any requests.
+app.listen(process.env.PORT || 3000, () =>
+  console.log('WorldNewsOrg app listening on port 3000!'),
+);
+
+
+
+
---
+++ Day 3/Homework3/src/routes/authorRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+import {getAuthors, addAuthor, wrongRoute} from "../controllers/authorController"
+
+const router  = express.Router()
+
+router.route("/authors")
+    .get(getAuthors)
+    .post(addAuthor)
+    .delete(wrongRoute)
+    .patch(wrongRoute)
+
+
+
+module.exports = router
